14cd4ef58d137c3ca4c5bf9dbdb45b260062497f
Chilllyy <chillywillymc@gmail.com>
2026-04-08 19:37:23 -0600

    create

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,75 @@
 package me.chillywilly
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import com.lightstreamer.client.ItemUpdate
+import com.lightstreamer.client.LightstreamerClient
+import com.lightstreamer.client.Subscription
+import com.lightstreamer.client.SubscriptionListener
+import io.javalin.Javalin
+import org.slf4j.LoggerFactory
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+var value = -1.0F
+var LOG = LoggerFactory.getLogger("ISSNormalizerServer")
+
+fun main() {
+    LOG.info("Starting normalizer server")
+    val client = LightstreamerClient("https://push.lightstreamer.com", "ISSLIVE")
+    client.connect()
+    LOG.info("Connected to Lightstreamer ${client.status}")
+
+    val sub = Subscription("MERGE", arrayOf("NODE3000005"), arrayOf("Value"))
+    sub.requestedSnapshot = "yes"
+    client.subscribe(sub)
+    sub.addListener(SubListener())
+    LOG.info("Added Lightstreamer Subscription")
+
+    val app = Javalin.create { config ->
+        config.http.asyncTimeout = 10000L
+        config.routes.get("/") { ctx ->
+            run {
+                LOG.info("Received Request for value")
+                ctx.result(value.toDouble().toString())
+            }
+        }
+    }.start(7000)
+}
+
+class SubListener : SubscriptionListener {
+    override fun onClearSnapshot(itemName: String?, itemPos: Int) {
+    }
+
+    override fun onCommandSecondLevelItemLostUpdates(lostUpdates: Int, key: String) {
+    }
+
+    override fun onCommandSecondLevelSubscriptionError(code: Int, message: String?, key: String?) {
+    }
+
+    override fun onEndOfSnapshot(itemName: String?, itemPos: Int) {
+    }
+
+    override fun onItemLostUpdates(itemName: String?, itemPos: Int, lostUpdates: Int) {
+    }
+
+    override fun onItemUpdate(itemUpdate: ItemUpdate) {
+        var newValue = itemUpdate.getValue("Value")?.toFloatOrNull() ?: value
+        value = newValue
+        LOG.info("Received New Value from Lightstreamer: ${newValue}")
+    }
+
+    override fun onListenEnd() {
+    }
+
+    override fun onListenStart() {
+    }
+
+    override fun onSubscription() {
+    }
+
+    override fun onSubscriptionError(code: Int, message: String?) {
+    }
+
+    override fun onUnsubscription() {
+    }
+
+    override fun onRealMaxFrequency(frequency: String?) {
     }
 }
